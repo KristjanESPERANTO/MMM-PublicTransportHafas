9ba98b6d8b87e15b588e3b10d0f62e88b7bc9825
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2026-01-01 20:35:48 +0100

    chore(changelog): change section label for chore type

--- changelog.config.js
+++ changelog.config.js
@@ -4,7 +4,7 @@ module.exports = {
     {type: "fix", section: "Fixed"},
     {type: "perf", section: "Performance Improvements"},
     {type: "docs", section: "Documentation"},
-    {type: "chore", section: "Changed"},
+    {type: "chore", section: "Chores"},
     {type: "refactor", section: "Code Refactoring"},
     {type: "test", section: "Tests"},
     {type: "build", section: "Build System"},
